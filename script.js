aa5f9056b94ee8d2b0a287bdc1248dbfd77c51e6
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2023-08-22 16:34:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+    const firstName=document.getElementsByClassName("fname");
+    const lastName=document.getElementsByClassName("lname");
+    alert(firstName.value);
+    alert(lastName.value);
 
 }
