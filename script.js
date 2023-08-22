94b97f505c8f848ecbc0cf30f5c6f874bbffd5aa
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2023-08-22 16:39:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function getFormvalue() {
     //Write your code here
-    const firstName=document.getElementsByClassName("fname");
-    const lastName=document.getElementsByClassName("lname");
-    alert(firstName.value);
-    alert(lastName.value);
+    // const firstName=document.getElementsByClassName("fname");
+    // const lastName=document.getElementsByClassName("lname");
+    // alert(firstName.value);
+    // alert(lastName.value);
+
+	event.preventDefault(); 
+            
+            var firstName = document.querySelector('input[name="fname"]').value;
+            var lastName = document.querySelector('input[name="lname"]').value;
+
+            alert("First Name: " + firstName + "\nLast Name: " + lastName);
 
 }
